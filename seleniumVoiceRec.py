71fa30a429b15ff11dbd617eaba4502f6ee1b7c8
Simon1PL <saicxs123@gmail.com>
2021-04-14 23:59:04 +0200

    add waiting for internet connection

--- seleniumVoiceRec.py
+++ seleniumVoiceRec.py
@@ -1,4 +1,7 @@
+import time
+
 from selenium import webdriver
+from datetime import datetime
 
 options = webdriver.ChromeOptions()
 # options.add_argument('headless') To psuje działanie programu
@@ -6,8 +9,27 @@ options.add_experimental_option("prefs", {
     "profile.default_content_setting_values.media_stream_mic": 1,
   })
 options.add_experimental_option("excludeSwitches", ["enable-logging"])
-browser = webdriver.Chrome(executable_path='E:/chromedriver.exe', chrome_options=options)
-browser.get('https://simon1pl.github.io/voiceController/')
+
+def openBrowser():
+    try:
+        browserRes = webdriver.Chrome(executable_path='E:/chromedriver.exe', chrome_options=options)
+        browserRes.get('https://simon1pl.github.io/voiceController/')
+        return browserRes
+    except Exception as e:
+        today = datetime.now()
+        f = open("pythonAppLogs.txt", "a")
+        f.write(str(today))
+        f.write(str(e))
+        f.close()
+        try:
+            browserRes.close()
+        except():
+            None
+        if "net::ERR_NAME_NOT_RESOLVED" in e.msg:
+            time.sleep(15)
+            return openBrowser()
+
+browser = openBrowser()
 browser.set_window_position(-10000, 0)
 browser.set_window_size(0, 0)
 browser.find_element_by_css_selector('.seleniumClick1').click()
