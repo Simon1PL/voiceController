973f002c112479c8a28a5a0e81c749e0999dfd2a
Simon1PL <saicxs123@gmail.com>
2021-04-14 15:38:06 +0200

    fix warning with bloototh connection? xD

--- seleniumVoiceRec.py
+++ seleniumVoiceRec.py
@@ -1,9 +1,11 @@
 from selenium import webdriver
 
 options = webdriver.ChromeOptions()
+# options.add_argument('headless') To psuje działanie programu
 options.add_experimental_option("prefs", {
     "profile.default_content_setting_values.media_stream_mic": 1,
   })
+options.add_experimental_option("excludeSwitches", ["enable-logging"])
 browser = webdriver.Chrome(executable_path='E:/chromedriver.exe', chrome_options=options)
 browser.get('https://simon1pl.github.io/voiceController/')
 browser.set_window_position(-10000, 0)
