6fcf9d2813c5ac8e9bd859ac9668814eaeac14ef
Simon1PL <saicxs123@gmail.com>
2021-04-14 03:48:23 +0200

    it works!

--- seleniumVoiceRec.py
+++ seleniumVoiceRec.py
@@ -5,7 +5,7 @@ options.add_experimental_option("prefs", {
     "profile.default_content_setting_values.media_stream_mic": 1,
   })
 browser = webdriver.Chrome(executable_path='E:/chromedriver.exe', chrome_options=options)
-browser.get('http://127.0.0.1:5501/index.html')
+browser.get('https://simon1pl.github.io/voiceController/')
 browser.set_window_position(-10000, 0)
 browser.set_window_size(0, 0)
 browser.find_element_by_css_selector('.seleniumClick1').click()
